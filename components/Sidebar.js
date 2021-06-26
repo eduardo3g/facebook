3256e5f5744815bd5dd9008055fd979d1dd3244f
eduardo3g <eduardosbrasil10@gmail.com>
2021-06-26 13:09:38 -0300

    fix: fix incorrect SidebarRow titles and icons

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -18,10 +18,11 @@ function Sidebar() {
   return (
     <div className="p-2 mt-5 max-w-[600px] xl:min-w-[300px]">
       <SidebarRow src={session.user.image} title={session.user.name} />
-      <SidebarRow Icon={UserGroupIcon} title="Friends" />
-      <SidebarRow Icon={ShoppingBagIcon} title="Groups" />
-      <SidebarRow Icon={DesktopComputerIcon} title="Marketplace" />
-      <SidebarRow Icon={CalendarIcon} title="Watch" />
+      <SidebarRow Icon={UsersIcon} title="Friends" />
+      <SidebarRow Icon={UserGroupIcon} title="Groups" />
+      <SidebarRow Icon={ShoppingBagIcon} title="Marketplace" />
+      <SidebarRow Icon={DesktopComputerIcon} title="Watch" />
+      <SidebarRow Icon={CalendarIcon} title="Events" />
       <SidebarRow Icon={ClockIcon} title="Memories" />
       <SidebarRow Icon={ChevronDownIcon} title="See More" />
     </div>
